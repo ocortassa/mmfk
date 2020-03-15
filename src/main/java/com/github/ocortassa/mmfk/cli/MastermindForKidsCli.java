72f2b0659a3fe7d56c42f9c64ac96f21668f883d
Omar Cortassa <omar.cortassa@gmail.com>
2020-03-15 15:57:10 +0100

    class refactoring and added unit test for cli

--- src/main/java/com/github/ocortassa/mmfk/cli/MastermindForKidsCli.java
+++ src/main/java/com/github/ocortassa/mmfk/cli/MastermindForKidsCli.java
@@ -16,47 +16,45 @@ public class MastermindForKidsCli {
 
     public static void main(String[] args) {
         MastermindForKidsCli cli = new MastermindForKidsCli();
-        cli.play();
-    }
-
-    public void play() {
-        outLn("");
-        boolean gameCompleted = false;
+        String secret = cli.readSecret();
+        for (int j = 0; j < 50; j++) {
+            cli.outLn("");
+        }
+        cli.setSecret(secret);
+        cli.outLn("Sequenza impostata, hai " + cli.getMaxAttempts() + " tentativi!\nSi comincia!");
         try {
+            cli.outLn("");
 
-            String secret = readSecret();
-            for (int j = 0; j < 50; j++) {
-                outLn("");
-            }
-            mmfk.setSecret(secret);
-            outLn("Sequenza impostata, hai " + mmfk.getMaxAttempts() + " tentativi!\nSi comincia!");
-
-            for (int i = 0; i < mmfk.getMaxAttempts(); i++) {
-                out("Tentativo " + (i + 1) + ", ");
-                String attempt = readAttempt();
-                String result = mmfk.hasWin(attempt);
-                if (result != null && result.isEmpty()) {
-                    gameCompleted = true;
-                    break;
+            for (int i = 0; i < cli.getMaxAttempts(); i++) {
+                cli.out("Tentativo " + (i + 1) + ", ");
+                String attempt = cli.readAttempt();
+                String result = cli.play(attempt);
+                if (result != null && !result.isEmpty()) {
+                    cli.outLn("HAI VINTO!!!!");
                 } else {
-                    outLn(result);
-                    //outLn("Peccato, non hai indovinato. Riprova!");
+                    cli.outLn("Peccato, non hai indovinato entro i " + cli.getMaxAttempts() + " tentativi previsti.\nLa sequenza giusta era: " + secret);
                 }
+                cli.outLn("");
             }   //  end game
 
-            if (gameCompleted) {
-                outLn("HAI VINTO!!!!");
-            } else {
-                outLn("Peccato, non hai indovinato entro i " + mmfk.getMaxAttempts() + " tentativi previsti.\nLa sequenza giusta era: " + secret);
-            }
-            outLn("");
-
         } catch (Exception e) {
-            logError(e.getMessage());
+            cli.outError(e.getMessage());
         }
     }
 
-    private String readSecret() {
+    public void setSecret(String secret) {
+        mmfk.setSecret(secret);
+    }
+
+    public int getMaxAttempts() {
+        return mmfk.getMaxAttempts();
+    }
+
+    public String play(String attempt) {
+        return mmfk.hasWin(attempt);
+    }
+
+    public String readSecret() {
         String secret;
         out("Inserisci sequenza segreta (R = Rosso, V = Verde, B = Blu, G = Giallo): ");
         Console console = System.console();
@@ -65,25 +63,28 @@ public class MastermindForKidsCli {
         } else {
             Scanner in = new Scanner(System.in);
             secret = in.next().trim();
+            in.close();
         }
         return secret;
     }
 
-    private String readAttempt() {
+    public String readAttempt() {
         Scanner in = new Scanner(System.in);
         out("vai: ");
-        return in.next().trim();
+        String value = in.next().trim();
+        in.close();
+        return value;
     }
 
-    private void out(String msg) {
+    public void out(String msg) {
         System.out.print(msg);
     }
 
-    private void outLn(String msg) {
+    public void outLn(String msg) {
         System.out.println(msg);
     }
 
-    private void logError(String msg) {
+    public void outError(String msg) {
         System.err.println(msg);
         LOGGER.error(msg);
     }
--- src/test/java/com/github/ocortassa/mmfk/test/MastermindForKidsCliTest.java
+++ src/test/java/com/github/ocortassa/mmfk/test/MastermindForKidsCliTest.java
@@ -1,16 +1,88 @@
 package com.github.ocortassa.mmfk.test;
 
 import com.github.ocortassa.mmfk.cli.MastermindForKidsCli;
-import org.junit.Assert;
+import org.junit.*;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class MastermindForKidsCliTest {
 
-    //@Test
-    public void doTest() {
-        // TODO: to complete!
+    @Rule
+    public final TextFromStandardInputStream systemInMock = TextFromStandardInputStream.emptyStandardInputStream();
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    private void resetBuffers() {
+        outContent.reset();
+        errContent.reset();
+    }
+
+    @Test
+    public void doPlayTest() {
         MastermindForKidsCli cli = new MastermindForKidsCli();
-        cli.play();
-        Assert.assertTrue(true);
+        Assert.assertEquals(12, cli.getMaxAttempts());
+        cli.setSecret("RRRR");
+        Assert.assertNotEquals(cli.play("RRRG"), "");
+        Assert.assertEquals(cli.play("RRRR"), "");
+    }
+
+    @Test
+    public void doReadSecretTest() {
+        MastermindForKidsCli cli = new MastermindForKidsCli();
+        systemInMock.provideLines("RRRR");
+        Assert.assertNotEquals("RRRG".trim(), cli.readSecret());
+        systemInMock.provideLines("RRRR");
+        Assert.assertEquals("RRRR".trim(), cli.readSecret());
+    }
+
+    @Test
+    public void doReadAttemptTest() {
+        MastermindForKidsCli cli = new MastermindForKidsCli();
+        systemInMock.provideLines("RRRR");
+        Assert.assertNotEquals("RRRG".trim(), cli.readAttempt());
+        systemInMock.provideLines("RRRR");
+        Assert.assertEquals("RRRR".trim(), cli.readAttempt());
+    }
+
+    @Test
+    public void doOutTest() {
+        MastermindForKidsCli cli = new MastermindForKidsCli();
+        resetBuffers();
+        cli.out("test");
+        Assert.assertEquals("test", outContent.toString());
+    }
+
+    @Test
+    public void doOutLnTest() {
+        MastermindForKidsCli cli = new MastermindForKidsCli();
+        resetBuffers();
+        cli.outLn("test");
+        Assert.assertEquals("test\r\n", outContent.toString());
+    }
+
+    @Test
+    public void doOutErrorTest() {
+        MastermindForKidsCli cli = new MastermindForKidsCli();
+        resetBuffers();
+        cli.outError("test");
+        Assert.assertEquals("test\r\n", errContent.toString());
     }
 
 }
